2d4b1d51d3df83644597a6cd447d8baeac768723
OscarB <obiondi99@gmail.com>
2022-04-22 17:29:37 -0500

    Añadida lectura de ram y creacion de csv

--- config/config.py
+++ config/config.py
@@ -1,3 +1,3 @@
 cron_hour = '*'
 cron_minute = '*/1'
-cron_second = '*/10'
+cron_second = '*/1'
--- src/logger.py
+++ src/logger.py
@@ -5,28 +5,44 @@ def run():
     
     year = datetime.now().year
     month = datetime.now().month
+    day = datetime.now().day
 
-    csv_path = f'./data/{year}_{month}_service_log.csv'
+    csv_path = f'./data/{year}_{month}_{day}_service_log.csv'
 
     """ Creacion de archivos """
     if exists(csv_path) == False:
-        _ = open(csv_path, 'w')
+        df = pd.DataFrame(columns = ['service_id','service_name','service_port','service_uptime','used_ram','total_ram','total_used_ram','total_free_ram','processor_used','datetime'])
+        df.loc[0] = ['','','','','','','','','','']
+        df.to_csv(csv_path, index=False)
 
     """ Correr los commands en la terminal y llevar el result a un archivo"""
     def log_services():
         services_result = os.popen('pm2 jlist').read()
-        ramcheck_result = os.popen('free -h -t').read()
-        print(ramcheck_result)
-        # json_res = json.loads(services_result)
-        # data = []
-        # for service in json_res:
-        #     """ Data structure: [ service_id, service_name, service_port, service_uptime, used_ram, total_used_ram, processor_used, datetime ] """
-        #     now = datetime.now().astimezone(pytz.timezone('America/Lima')).timestamp()
-        #     name, port = str(service["name"]).split(':')
-        #     data.append([ service["pm_id"], name, port, service["pm2_env"]["pm_uptime"], service["monit"]["memory"], 0, service["monit"]["cpu"], int(now) ])
-        # df = pd.DataFrame(data)
-        # df.columns = [ 'service_id', 'service_name', 'service_port', 'service_uptime', 'used_ram', 'total_used_ram', 'processor_used', 'datetime' ]
-        # df.to_csv(csv_path)
+        ramcheck_result = os.popen('free -t').read()
+        try:
+            splitted = ramcheck_result.split("\n")
+            total_stuff : list = []
+            for row in splitted:
+                allvalue = []
+                values = str(row).strip().split(' ')
+                values = list(filter(None, values))
+                total_stuff.append(values)
+            ram_data = list(total_stuff[-1]).pop(0)
+            total_ram = ram_data[0]
+            used_ram = ram_data[1]
+            free_ram = ram_data[2]
+        except:
+            total_ram = 0
+            used_ram = 0
+            free_ram = 0
+        data = []
+        for service in services_result:
+            """ Data structure: [ service_id, service_name, service_port, service_uptime, used_ram, total_used_ram, processor_used, datetime ] """
+            now = datetime.now().astimezone(pytz.timezone('America/Lima')).timestamp()
+            name, port = str(service["name"]).split(':')
+            data.append([service["pm_id"],name,port,service["pm2_env"]["pm_uptime"],service["monit"]["memory"],total_ram,used_ram,free_ram,service["monit"]["cpu"],int(now)])
+        df = pd.DataFrame(data)
+        df.to_csv(csv_path, mode='a', index=False, header=False)
 
     log_services()
             
